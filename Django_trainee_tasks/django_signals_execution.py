573a042a80bd51b07646048ae3af2e46ef17d451
Prajakta17-bod <prajaktabodade2020@gmail.com>
2024-10-30 18:25:46 +0500

    Create django_signals_execution.py

---
+++ Django_trainee_tasks/django_signals_execution.py
@@ -0,0 +1,17 @@
+from django.dispatch import Signal, receiver
+import time
+
+my_signal = Signal()
+
+
+@receiver(my_signal)
+def my_receiver(sender, **kwargs):
+    print("Receiver started")
+    time.sleep(2)  
+    print("Receiver finished")
+
+print("Sending signal...")
+my_signal.send(sender=None)
+print("Signal sent.")
+
+
